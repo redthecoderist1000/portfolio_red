e2a498c8bbff87f56cb48437fddbe5c040e839ea
redthecoderist1000 <redochavillo@gmail.com>
2024-11-11 15:28:06 +0800

    tech

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,26 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import "./css/navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <Navbar expand="lg" className="nav_bar bg-body-tertiary ">
+    <Navbar sticky="top" expand="lg" className="nav_bar">
       <Container>
-        <Navbar.Brand href="/">red ochavillo</Navbar.Brand>
+        <Navbar.Brand>
+          {" "}
+          <Link to="/">red ochavillo</Link>{" "}
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="tabs">
-            <Nav.Link href="works">works</Nav.Link>
-            <Nav.Link href="contact">contact</Nav.Link>
+            <Nav.Link>
+              <Link to="/work">work</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/contact">contact</Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,32 @@ import NavBar from "../components/NavBar";
 import { Container, Row } from "react-bootstrap";
 import "./css/home.css";
 import profile from "../assets/profile.jpg";
-import Button from "react-bootstrap/Button";
+import html from "../assets/tech_logo/html.png";
+import css from "../assets/tech_logo/css.png";
+import php from "../assets/tech_logo/php.png";
+import java from "../assets/tech_logo/java.png";
+import react from "../assets/tech_logo/react.png";
+import js from "../assets/tech_logo/js.png";
+import flutter from "../assets/tech_logo/flutter.png";
+import sql from "../assets/tech_logo/sql.png";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
 export default function Home() {
+  const tech_logo = [java, html, css, php, js, sql, react, flutter];
+
   return (
     <>
       <NavBar />
-      <Container>
-        <h1 className="text-center">red ochavillo</h1>
-        <div className="d-flex justify-content-evenly">
-          <p>text</p>
-          <p>text</p>
-          <p>text</p>
-        </div>
-        <Row className="justify-content-evenly" xs={2} md={4}>
+      <Container className="home_con mt-5">
+        <h1 className="name m-0">red zinfandel ochavillo</h1>
+        <Row className="position_row mt-4" xs={2} md={4}>
+          <p>job title</p>
+          <p>job title</p>
+          <p>job title</p>
+        </Row>
+        <Row className="about_row mt-5" xs={2} md={4}>
           <Col>
             <Image src={profile} rounded />
           </Col>
@@ -34,6 +43,13 @@ export default function Home() {
               </Card.Text>
             </Card.Body>
           </Card>
+        </Row>
+        <Row className="tech mt-5 " xs={2} md={4}>
+          {tech_logo.map((data) => (
+            <Col style={{ width: "fit-content" }} xs={6} md={4}>
+              <Image src={data} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
